6c66083eef6c5a1e5acf759daab7340b5fd8f083
fachhhh <fachhhhs.01@gmail.com>
2024-09-17 00:50:44 +0700

    make function for show xml and json then set path for function in views.py

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from main.forms import EcommerceEntryForm
 from main.models import Ecommerce
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     product_entries = Ecommerce.objects.all()
@@ -23,3 +25,25 @@ def create_product_entry(request):
     
     context = {'form':form}
     return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = Ecommerce.objects.all()
+
+def show_xml(request):
+    data = Ecommerce.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Ecommerce.objects.all()
+
+def show_json(request):
+    data = Ecommerce.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Ecommerce.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Ecommerce.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
