dc10b252dc14ef4d94dc0236e50cb42fe37bb6ef
fachhhh <fachhhhs.01@gmail.com>
2024-09-16 22:40:20 +0700

    Make migration and migrate after edit models.py then make forms.py

--- main/models.py
+++ main/models.py
@@ -2,8 +2,9 @@ from django.db import models
 import uuid
 
 class Ecommerce(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+    price = models.DecimalField(max_digits=13, decimal_places=3) # harga maks adalah dalam satuan miliar dengan 3 decimal
     quantity = models.IntegerField()
     description = models.TextField()
 
