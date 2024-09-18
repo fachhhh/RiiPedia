e9e6b067275ca2625c85266085c92389067853df
fachhhh <fachhhhs.01@gmail.com>
2024-09-18 12:07:24 +0700

    buat tes aja

--- RiiPedia/settings.py
+++ RiiPedia/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://hadyan-fachri-riipedia.pbp.cs.ui.ac.id/", "https://hadyan-fachri-riipedia.pbp.cs.ui.ac.id/"]
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://hadyan-fachri-riipedia.pbp.cs.ui.ac.id/", "https://hadyan-fachri-riipedia.pbp.cs.ui.ac.id/"]
+#CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1"]
