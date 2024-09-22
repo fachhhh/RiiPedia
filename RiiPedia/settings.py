ba9f35343bc2b9f75549b7511b80fc979dd0bf14
fachhhh <fachhhhs.01@gmail.com>
2024-09-22 07:25:40 +0700

    Add PRODUCTION and set DEBUG on settings.py when deploy to pws

--- RiiPedia/settings.py
+++ RiiPedia/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8ml_54$%s2+39p^t0k6=6@2k!1vpec37^3))(+eusj(&xs8n81'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "hadyan-fachri-riipedia.pbp.cs.ui.ac.id"]
 
